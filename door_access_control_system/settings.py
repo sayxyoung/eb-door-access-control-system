fd67165eef86c5741ab4b3d4c86bbd785065fc06
Ubuntu <ubuntu@ip-172-31-27-195.us-east-2.compute.internal>
2021-05-14 10:06:34 +0000

    Modify : indent in settings.py

--- door_access_control_system/settings.py
+++ door_access_control_system/settings.py
@@ -81,17 +81,17 @@ WSGI_APPLICATION = 'door_access_control_system.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
- if 'RDS_HOSTNAME' in os.environ:
-     DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.mysql',
-             'NAME': os.environ['RDS_DB_NAME'],
-             'USER': os.environ['RDS_USERNAME'],
-             'PASSWORD': os.environ['RDS_PASSWORD'],
-             'HOST': os.environ['RDS_HOSTNAME'],
-             'PORT': os.environ['RDS_PORT'],
-         }
-     }
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+        }
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.mysql',
